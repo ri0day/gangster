1a1ba99ef7920ff106f1fdcdd69229eb2a8f68fb
ri0day <hackcrisman@gmail.com>
2017-11-20 16:56:47 +0800

    lxml better than xml

--- qunar.py
+++ qunar.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*- 
-import xml.etree.ElementTree as etree
-import urllib
-context = urllib.urlopen('http://ws.qunar.com/holidayService.jcp?lane=上海-长沙')
-tree = etree.parse(context)
-root = tree.getroot()
+#!/usr/bin/env python
+#coding: utf-8
+import requests
+from lxml import  etree
+url = 'http://ws.qunar.com/holidayService.jcp?lane=%E4%B8%8A%E6%B5%B7-%E9%95%BF%E6%B2%99'
+r = requests.get(url).text.encode('utf-8')
+root = etree.fromstring(r)
+
 for node in root[0]:
-        if node.attrib["date"] == "2011-09-30":
+        if node.attrib["date"] in [ "2018-02-10","2018-02-11","2018-02-12" ]:
                 for child in node:
                         for child_detail in child.attrib.keys():
-                                if child.attrib["type"] == "go" and int(child.attrib["price"])<600:
+                                 if child.attrib["type"] == "go" and int(child.attrib["price"])<=650:
                                         print child_detail,child.attrib[child_detail]
-                                        urllib.urlopen('http://api.sms.xxx.com/sms.jcp?c="有机票了"')
+
